6a6d28f3764f74008ab658bf4ebf62b00c48e991
Kazumasa Shimomura <kazu.devapp@gmail.com>
2017-10-21 10:28:15 +0900

    project initialize

---
+++ stress/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"os"
+	"reflect"
+)
+
+type configManager struct {
+	PORT string
+}
+
+var sharedInstance *configManager = newConfigManager()
+
+func newConfigManager() *configManager {
+	port := os.Getenv("PORT")
+
+	//FIXME: sliceを使わなくてもいいようにしたい
+	slice := []string{port}
+	for i := 0; i < len(slice); i++ {
+		if slice[i] == "" {
+			panic("[FATAL]" + reflect.ValueOf(configManager{}).Type().Field(i).Name + " is not assign")
+		}
+	}
+git
+	return &configManager{port}
+}
+
+func GetInstance() *configManager {
+	return sharedInstance
+}
