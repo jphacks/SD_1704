968a67a236be449627391bb9aec7054d84f62727
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2017-10-21 15:14:17 +0900

    音声の取得 shout.htmlの作成 音声を取得しプログレスバーで描画

--- stress/handler/handler.go
+++ stress/handler/handler.go
@@ -9,3 +9,7 @@ import (
 func RootHandler(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", gin.H{})
 }
+
+func ShoutHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "shout.html", gin.H{})
+}
--- stress/manager/server.go
+++ stress/manager/server.go
@@ -8,9 +8,12 @@ import (
 
 func Init() {
 	r := gin.Default()
+
 	r.LoadHTMLGlob("templates/*.html")
+	r.LoadHTMLGlob("templates/js/*.html")
 
 	r.GET("/", handler.RootHandler)
+	r.GET("/shout", handler.ShoutHandler)
 
 	r.Run(":" + config.GetInstance().PORT) // listen and serve on 0.0.0.0:8080
 }
