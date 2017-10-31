f941d17c5f281e60d736e414184f42192366bdaa
Kazumasa Shimomura <kazu.devapp@gmail.com>
2017-11-01 00:18:40 +0900

    [modify]use database url

--- vendor/github.com/jphacks/SD_1704/stress/config/config.go
+++ vendor/github.com/jphacks/SD_1704/stress/config/config.go
@@ -14,8 +14,8 @@ var sharedInstance *configManager = newConfigManager()
 
 func newConfigManager() *configManager {
 	port := os.Getenv("PORT")
-	//dbUrl := os.Getenv("DATABASE_URL")
-	dbUrl := "dbname=stress sslmode=disable"
+	dbUrl := os.Getenv("DATABASE_URL")
+	//dbUrl := "dbname=stress sslmode=disable"
 
 	//FIXME: sliceを使わなくてもいいようにしたい
 	slice := []string{port}
