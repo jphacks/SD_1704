09bd3f26718c354d58d9a0d5e1da460185d27334
Kazumasa Shimomura <kazu.devapp@gmail.com>
2017-11-01 01:00:58 +0900

    [modify]database url

--- config/config.go
+++ config/config.go
@@ -14,8 +14,8 @@ var sharedInstance *configManager = newConfigManager()
 
 func newConfigManager() *configManager {
 	port := os.Getenv("PORT")
-	//dbUrl := os.Getenv("DATABASE_URL")
-	dbUrl := "dbname=stress sslmode=disable"
+	dbUrl := os.Getenv("HEROKU_POSTGRESQL_SILVER_URL")
+	//dbUrl := "dbname=stress sslmode=disable"
 
 	//FIXME: sliceを使わなくてもいいようにしたい
 	slice := []string{port}
